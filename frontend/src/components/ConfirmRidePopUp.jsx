51564c8e1d460cc0f2a67022dfd583e2544da56e
Panchali-Das <dpanchali817@gmail.com>
2025-07-16 02:32:09 +0530

    captain UI complete

---
+++ frontend/src/components/ConfirmRidePopUp.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const ConfirmRidePopUp = (props) => {
+  const [otp, setOtp] = useState("");
+  const submitHander = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <h5
+        className="p-1 text-center w-[93%] absolute top-0"
+        onClick={() => {
+          props.setRidePopupPanel(false);
+        }}
+      >
+        <i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
+      </h5>
+      <h3 className="text-2xl font-semibold mb-5">
+        Confirm this ride to Start
+      </h3>
+      <div className="flex items-center justify-between p-3 bg-yellow-400 rounded-lg mt-4">
+        <div className="flex items-center gap-3">
+          <img
+            className="h-12 rounded-full object-cover w-10"
+            src="https://img.freepik.com/free-photo/girl-with-long-hair-being-happy_23-2148244714.jpg?semt=ais_hybrid&w=740"
+            alt=""
+          />
+          <h2 className="text-lg font-medium">Harsha Patel</h2>
+        </div>
+        <h5 className="text-lg font-semibold">2.2 KM</h5>
+      </div>
+      <div className="flex gap-2 justify-between flex-col items-center">
+        <div className="w-full mt-5">
+          <div className="flex items-center gap-5 p-3 border-b-2 border-white">
+            <i className="ri-map-pin-user-line"></i>
+            <div>
+              <h3 className="text-lg font-medium">562/11-A</h3>
+              <p className="text-sm -ml-1 text-gray-600">
+                Kankariya Talab, Bhopal
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3 border-b-2 border-white">
+            <i className="text-lg ri-map-pin-2-fill"></i>
+            <div>
+              <h3 className="text-lg font-medium">562/11-A</h3>
+              <p className="text-sm -ml-1 text-gray-600">
+                Kankariya Talab, Bhopal
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3 ">
+            <i className="ri-currency-line"></i>
+            <div>
+              <h3 className="text-lg font-medium">₹193.20</h3>
+              <p className="text-sm -ml-1 text-gray-600">Cash Cash</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6 w-full">
+          <form
+            onSubmit={(e) => {
+              submitHander(e);
+            }}
+          >
+            <input
+              value={otp}
+              onChange={(e) => setOtp(e.target.value)}
+              type="text"
+              className="bg-[#eee] px-6 py-4 font-mono text-lg rounded-lg w-full mt-3"
+              placeholder="Enter OTP"
+            />
+            <Link
+              to="/captain-riding"
+              onClick={() => {}}
+              className="w-full mt-5 text-lg flex justify-center bg-green-600 text-white font-semibold p-3 rounded-lg"
+            >
+              Confirm
+            </Link>
+            <button
+              onClick={() => {
+                props.setConfirmRidePopupPanel(false);
+                props.setRidePopupPanel(false);
+              }}
+              className="w-full mt-1 bg-red-600 text-lg text-white font-semibold p-3 rounded-lg"
+            >
+              Cancel
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmRidePopUp;
